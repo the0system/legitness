7855839584a042ca011272871b3d259c3ab47713
the0system <dawidyhisreal@hotmail.com>
2016-01-18 17:48:27 -0600

    Initial commit

---
+++ src/main/java/theExchange/legitness/editor/LegitnessApplication.java
@@ -0,0 +1,12 @@
+package theExchange.legitness.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegitnessApplication.class, args);
+	}
+}
---
+++ src/test/java/theExchange/legitness/editor/LegitnessApplicationTests.java
@@ -0,0 +1,16 @@
+package theExchange.legitness.editor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LegitnessApplication.class)
+public class LegitnessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
